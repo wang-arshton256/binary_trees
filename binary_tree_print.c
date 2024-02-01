5ddc7eca3fa1354ee060b94aa80c120c78c45800
Wangutusi Arshton <wangarshton@gmail.com>
2024-02-02 01:38:26 +0300

    fixed error

---
+++ binary_tree_print.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node.
+ * @value: value to put in the new node.
+ * @parent: Pointer to the parent of the node to create.
+ *
+ * Return: If an error occurs - NULL.
+ * otherwise - a pointer to the new node.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+
+    new = malloc(sizeof(binary_tree_t));
+    if (new == NULL)
+        return (NULL);
+
+    new->n = value;
+    new->parent = parent;
+    new->left = NULL;
+    new->right = NULL;
+
+    return (new);
+}
